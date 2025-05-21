48778d22ac9dc14e4968cd0fbbe887b9c35b8983
TristanSaidi <83875235+TristanSaidi@users.noreply.github.com>
2025-05-21 22:41:46 +0000

    Deploying to gh-pages from @ TristanSaidi/TristanSaidi.github.io@fce54697842122d9877b781315f16c31a9c54fe4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -454,13 +454,6 @@ ninja.data = [{
           window.open("https://orcid.org/0009-0007-9577-5210", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
