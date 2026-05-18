37f18f516d6b7a559db3e57a26e56c2ab74bba65
TristanSaidi <83875235+TristanSaidi@users.noreply.github.com>
2026-05-18 04:29:47 +0000

    Deploying to gh-pages from @ TristanSaidi/TristanSaidi.github.io@e6e88a66a549c624d6e0c1a0b33beaca60a62b5b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -53,22 +53,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-talk-at-cmu-causal-inference-group",
-          title: 'Talk at CMU Causal Inference group 🎤',
+            },},{id: "news-talk-at-cmu-causal-inference-group",
+          title: '🎤 Talk at CMU Causal Inference group',
           description: "",
           section: "News",},{id: "news-quantitative-research-intern-at-citadel-securities",
-          title: 'Quantitative research intern at Citadel Securities',
+          title: '🧳 Quantitative research intern at Citadel Securities',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
